b50601d1020f1cca491f617bf6ba4723e1d5224a
Quilver <saurothe@gmail.com>
2024-05-17 13:20:53 +0100

    Outlined AI behaviour system

---
+++ Assets/Battle Scripts/AI behaviours/Unit AI/UnitAI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace UnitAISystem
+{
+    public class UnitAI : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+
+        }
+        private void OnDisable()
+        {
+
+        }
+        void UpdateAI()
+        {
+
+        }
+        int currentPriority;
+        Vector2Int target;
+        public void SetMove(int prioirity, Vector2Int location)
+        {
+
+        }
+
+    }
+}
+
---
+++ Assets/Battle Scripts/AI behaviours/Unit AI/UnitBehaviour.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace UnitAISystem
+{
+    public abstract class UnitBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        protected int priority;
+
+    }
+}
+
