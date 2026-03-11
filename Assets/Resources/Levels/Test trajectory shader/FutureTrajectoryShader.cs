a9035a253bfcc545c10afecb42c105cc866e7a40
Quinte <saurothe@gmail.com>
2026-03-11 22:30:20 +0000

    Initialized new shader

--- Assets/Resources/Levels/Test trajectory shader/FutureTrajectoryShader.cs
+++ Assets/Resources/Levels/Test trajectory shader/FutureTrajectoryShader.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace ComputeShader
+namespace ComputeShaderTest
 {
     public class FutureTrajectoryShader : MonoBehaviour
     {
 
-        public List<Rigidbody2D> staticBodies, dynamicBodies;
+        public Rigidbody2D body;
         public float timeStep = 2f;
+
+        //Compute shader variables
+        public ComputeShader trajectoryComputeShader;
+        public RenderTexture trajectoryTexture;
         struct ObjectData
         {
             public Vector2 position;
@@ -18,13 +22,33 @@ namespace ComputeShader
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-            
+            trajectoryTexture = new RenderTexture(256, 256, 4);
+            trajectoryTexture.enableRandomWrite = true;
+            trajectoryTexture.Create();
+
+            InitBuffer();
         }
-
-        // Update is called once per frame
-        void Update()
+        void InitBuffer()
         {
-
+            trajectoryComputeShader.SetTexture(0, "Pixels", trajectoryTexture);
+            Mesh mesh = body.GetComponent<Collider2D>().CreateMesh(false, false);
+            //trajectoryComputeShader.SetBuffer(0, "objectMesh", mesh.vertexBufferTarget);
+            trajectoryComputeShader.SetFloat("Radius", 1);
+            trajectoryComputeShader.SetFloat("Resolution", 1);
+        }
+        void UpdateBuffer()
+        {
+            trajectoryComputeShader.SetFloat("timeStep", timeStep);
+            trajectoryComputeShader.SetVector("Position", body.position);
+            trajectoryComputeShader.SetVector("Velocity", body.linearVelocity);
+            trajectoryComputeShader.SetFloat("Rotation", body.angularVelocity);
+        }
+        private void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            if(trajectoryTexture == null) return;
+            UpdateBuffer();
+            trajectoryComputeShader.Dispatch(0, trajectoryTexture.width / 8, trajectoryTexture.height / 8, 1);
+            Graphics.Blit(trajectoryTexture, destination);
         }
     }
 }
