52574a48e3ff7055563ea56a106f4655d8207d41
Quilver <saurothe@gmail.com>
2026-03-12 13:22:43 +0000

    projection shader debugging

--- Assets/Resources/Levels/Test trajectory shader/FutureTrajectoryShader.cs
+++ Assets/Resources/Levels/Test trajectory shader/FutureTrajectoryShader.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,43 +13,55 @@ namespace ComputeShaderTest
         //Compute shader variables
         public ComputeShader trajectoryComputeShader;
         public RenderTexture trajectoryTexture;
-        struct ObjectData
-        {
-            public Vector2 position;
-            public Vector2 velocity;
-            public float angularVelocity;
-            public float radius; // Based on collider size
-        }
+        int kernelID;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-            trajectoryTexture = new RenderTexture(256, 256, 4);
+            trajectoryTexture = new RenderTexture(256, 256, 24);
+            trajectoryTexture.format = RenderTextureFormat.ARGB32;
             trajectoryTexture.enableRandomWrite = true;
             trajectoryTexture.Create();
-
+           
             InitBuffer();
+            Sprite dynamicSprite = Sprite.Create(
+        // We use Texture2D.CreateExternalTexture to treat the RT as a texture
+        Texture2D.CreateExternalTexture(
+            trajectoryTexture.width,
+            trajectoryTexture.height,
+            TextureFormat.RGBA32,
+            false, false,
+            trajectoryTexture.GetNativeTexturePtr()),
+        new Rect(0, 0, trajectoryTexture.width, trajectoryTexture.height),
+        new Vector2(0.5f, 0.5f)
+    );
+            GetComponent<SpriteRenderer>().sprite= dynamicSprite;
         }
         void InitBuffer()
         {
-            trajectoryComputeShader.SetTexture(0, "Pixels", trajectoryTexture);
+            kernelID = trajectoryComputeShader.FindKernel("ProjectionMap");
+            trajectoryComputeShader.SetTexture(kernelID, "_Pixels", trajectoryTexture);
             Mesh mesh = body.GetComponent<Collider2D>().CreateMesh(false, false);
-            //trajectoryComputeShader.SetBuffer(0, "objectMesh", mesh.vertexBufferTarget);
-            trajectoryComputeShader.SetFloat("Radius", 1);
-            trajectoryComputeShader.SetFloat("Resolution", 1);
+            ComputeBuffer collisionMeshBuffer = new ComputeBuffer(mesh.vertexCount, sizeof(float) * 3);
+            Vector3[] vertices = mesh.vertices;
+            collisionMeshBuffer.SetData(vertices);
+            trajectoryComputeShader.SetBuffer(kernelID, "_CollisionMesh", collisionMeshBuffer);
+            trajectoryComputeShader.SetInt("_TriangleCount", mesh.vertexCount / 3);
+            trajectoryComputeShader.SetFloat("_Radius", 32);
+            trajectoryComputeShader.SetFloat("_Resolution", 256);
         }
         void UpdateBuffer()
         {
-            trajectoryComputeShader.SetFloat("timeStep", timeStep);
-            trajectoryComputeShader.SetVector("Position", body.position);
-            trajectoryComputeShader.SetVector("Velocity", body.linearVelocity);
-            trajectoryComputeShader.SetFloat("Rotation", body.angularVelocity);
+            trajectoryComputeShader.SetFloat("_Time", timeStep);
+            trajectoryComputeShader.SetVector("_Position", body.position);
+            trajectoryComputeShader.SetVector("_Velocity", body.linearVelocity);
+            trajectoryComputeShader.SetFloat("_Rotation", body.angularVelocity);
         }
-        private void OnRenderImage(RenderTexture source, RenderTexture destination)
+        private void Update()
         {
-            if(trajectoryTexture == null) return;
+            if (trajectoryTexture == null) return;
             UpdateBuffer();
-            trajectoryComputeShader.Dispatch(0, trajectoryTexture.width / 8, trajectoryTexture.height / 8, 1);
-            Graphics.Blit(trajectoryTexture, destination);
+            transform.position = body.position;
+            trajectoryComputeShader.Dispatch(kernelID, trajectoryTexture.width / 8, trajectoryTexture.height / 8, 1);
         }
     }
 }
