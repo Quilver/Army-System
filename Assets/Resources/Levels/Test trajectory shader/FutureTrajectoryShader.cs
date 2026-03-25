3c40f3d4d7d8ea4254aaf252a24e05328706e992
Quinte <saurothe@gmail.com>
2026-03-25 21:26:48 +0000

    Shader debugging

--- Assets/Resources/Levels/Test trajectory shader/FutureTrajectoryShader.cs
+++ Assets/Resources/Levels/Test trajectory shader/FutureTrajectoryShader.cs
@@ -31,25 +31,40 @@ namespace ComputeShaderTest
         public int steps = 10;
         public Vector2 pixelOffset=new Vector2(-1,-0.5f);
         public Vector2 RotatedPos, RotatedDir, nextPos;
+        Vector2 AngularVelocity(float angle)
+        {
+            return new Vector2(-Mathf.Sin(angle * Mathf.Deg2Rad), Mathf.Cos(angle * Mathf.Deg2Rad));
+        }
         void DrawLine(Vector2 point, bool onRect)
         {
-            Vector2 vel = (onRect) ? body.linearVelocity : -body.linearVelocity;
-            float angularVel = (onRect) ? body.angularVelocity : -body.angularVelocity;
+            
+            Vector2 vel = (onRect) ? body.linearVelocity*timeStep : -body.linearVelocity*timeStep;
+            float angularVel = (onRect) ? body.angularVelocity*timeStep : -body.angularVelocity * timeStep;
             float angle = (onRect) ? body.transform.rotation.eulerAngles.z : -body.transform.rotation.eulerAngles.z;
+            Vector2 angleVel = AngularVelocity(angle)*timeStep;
+
+            //actual starting position and velocity
             Gizmos.color = Color.white;
             Gizmos.DrawSphere(body.position + point, 0.1f);
-            Gizmos.DrawRay(body.position + point, -body.linearVelocity);
-            Gizmos.color = Color.yellow;
+            Gizmos.DrawRay(body.position + point+angleVel, vel);
+            Gizmos.DrawRay(body.position + point, angleVel);
 
+            //Actual end position
+            Gizmos.color = Color.blue;
+            Vector2 endpos = point + angleVel + vel;
+                //Quaternion.AngleAxis(body.rotation + angularVel, Vector3.forward) * point + (Vector3)vel;
+            Gizmos.DrawSphere(body.position + endpos, 0.1f);
+            //Rotated starting position and velocity
+            Gizmos.color = Color.yellow;
             RotatedPos = (onRect)?body.transform.rotation * point : Quaternion.Inverse(body.transform.rotation) * point;
             RotatedDir = (onRect) ? body.transform.rotation * vel : Quaternion.Inverse(body.transform.rotation) * vel;
             Gizmos.DrawWireCube(body.transform.position, body.transform.localScale);
             Gizmos.DrawSphere(body.position + RotatedPos, 0.1f);
-            Gizmos.DrawRay(body.position + RotatedPos, -RotatedDir);
+            Gizmos.DrawRay(body.position + RotatedPos, RotatedDir);
             Gizmos.color = Color.red;
-            nextPos = Quaternion.AngleAxis(body.rotation + body.angularVelocity, Vector3.forward) * (RotatedPos);
-            //Gizmos.DrawSphere(body.position + nextPos, 0.1f);
-            //Gizmos.DrawLine(body.position + RotatedPos, body.position + nextPos);
+            nextPos = Quaternion.AngleAxis(body.rotation + angularVel, Vector3.forward) * (RotatedPos) +(Vector3)vel;
+            Gizmos.DrawSphere(body.position + nextPos, 0.1f);
+            Gizmos.DrawLine(body.position + RotatedPos, body.position + nextPos);
         }
         private void OnDrawGizmos()
         {
